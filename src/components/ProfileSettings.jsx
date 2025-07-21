0d56b5715504f880b84358a2ca7cdc426e9eef47
william <wilsher987@gmail.com>
2025-07-21 10:57:11 -0500

    commit_ERModels_finish

--- src/components/ProfileSettings.jsx
+++ src/components/ProfileSettings.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import './ModelosER.css';
 
 // Importaciones con los nombres exactos de tu carpeta
-import zoologicos from './Problema_1_Zoológicos.png';
-import abogados from './Diagrama_ER_Gabinete_De_Abogados.png';
-import ventas from './Diagrama_ER_de_Sistema_de_Ventas.png';
-import uvi from './UVI.png';
+import zoologicos from './Problema-Zoologicos.png';
+import abogados from './diagrama-er-gabinete-abogados.png';
+import ventas from './diagrama-er-sistema-ventas.png';
+import uvi from './Diagrama-UVI.png';
 
 const diagramas = [
-  { nombre: "Problema Zoológicos", archivo: zoologicos },
-  { nombre: "Diagrama ER, Gabinete de Abogados", archivo: abogados },
-  { nombre: "Diagrama ER de Sistemas de Ventas", archivo: ventas },
-  { nombre: "Diagrama ER de bases de datos (enunciado UVI)", archivo: uvi },
+  { nombre: "Problema Zoologicos", archivo: zoologicos },
+  { nombre: "Diagrama Gabinete de Abogados", archivo: abogados },
+  { nombre: "Diagrama Sistemas de Ventas", archivo: ventas },
+  { nombre: "Diagrama (enunciado UVI)", archivo: uvi },
 ];
 
 export default function ProfileSettings() {
